e91f786546fc3e9ca14f08b81f2200f95822dc05
Capi /. Etheriel <barraponto@gmail.com>
2011-01-10 09:58:51 -0200

    added debug options

--- config.rb
+++ config.rb
@@ -7,6 +7,9 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
+
+sass_options = {:debug_info => true}
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 relative_assets = true
